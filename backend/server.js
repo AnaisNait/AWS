d4cdb21f8d3a194ea28e38c53ee3ae03ec979bea
uvsq22307849 <boualirami850@gmail.com>
2024-04-11 23:59:08 +0200

    ajouter la route de virement dans le serveur

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
+import cors from 'cors';
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-// import produitRouter from "./routes/produits.js";
-import cors from 'cors';
 import benefRouter from "./routes/benefs.js";
+import rendezvousRouter from './routes/rendezvous.js';
 import soldeRouter from "./routes/soldes.js";
-import authRouter from "./routes/users.js";
+import { default as authRouter, default as userRouter } from "./routes/users.js";
+import virementRouter from "./routes/virements.js";
 
 dotenv.config();
 
@@ -13,30 +14,40 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Route principale avec un message de bienvenue
 app.get('/', (request, response, next) => {
     console.log(request)
-    return response.status(234).send('welcome home')
+    return response.status(234).send('Bienvenue')
 
 });
 
-// authentication
+// Authentification
 app.use('/auth', authRouter);
 
+// Routes utilisateur
+app.use('/user', userRouter);
+
+// Routes bénéficiaire
 app.use('/beneficiaire', benefRouter);
 
-app.use('/solde',soldeRouter)
+// Routes solde
+app.use('/solde', soldeRouter);
 
+// Routes virement
+app.use('/virement', virementRouter);
 
+// Routes rendez-vous
+app.use('/rendezvous', rendezvousRouter);
+
+// Connexion à MongoDB
 mongoose
     .connect(process.env.mongoDBURL)
     .then(() => {
-        console.log('app connected to database')
+        console.log('Application connectée à la base de données')
         app.listen(process.env.PORT, () => {
-            console.log(`App is listening to port: ${process.env.PORT}`);
+            console.log(`L'application écoute sur le port: ${process.env.PORT}`);
         });
     })
-
     .catch((error) => {
         console.log(error)
-    })
-
+    });
