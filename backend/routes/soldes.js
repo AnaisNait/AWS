ac702777166ec5655b7818a9a9255989a7975281
uvsq22307849 <boualirami850@gmail.com>
2024-04-11 23:57:19 +0200

    fixer les routes et ajouter des commentaires

--- backend/routes/soldes.js
+++ backend/routes/soldes.js
@@ -1,17 +1,15 @@
 import express from "express";
-import { afficherSolde, ajouterSolde } from "../controllers/solde.js";
-import checkAuth from "../middlewares/check-auth.js";
+import { afficherSolde, ajouterSolde } from "../controllers/solde.js"; // Import des contrôleurs pour les opérations sur le solde
+import checkAuth from "../middlewares/check-auth.js"; // Middleware pour vérifier l'authentification
 
+const soldeRouter = express.Router(); // Création du routeur pour le solde
 
-const soldeRouter = express.Router();
+soldeRouter.use(checkAuth); // Utilisation du middleware pour vérifier l'authentification sur toutes les routes de ce routeur
 
-soldeRouter.use(checkAuth);
-
+// Route pour afficher le solde de l'utilisateur
 soldeRouter.get('/afficherSolde', afficherSolde);
 
-
+// Route pour ajouter du solde à celui de l'utilisateur
 soldeRouter.put('/ajouterSolde', ajouterSolde);
 
-
-
-export default soldeRouter
+export default soldeRouter; // Export du routeur de solde
