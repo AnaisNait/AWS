1c9e5bf573c06e30160e12d122de34349c384c8c
uvsq22307849 <boualirami850@gmail.com>
2024-04-12 00:04:11 +0200

    changer l'icone pour l'implémenter dans la page principale

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,12 +1,12 @@
-import { BsArrowLeft } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
+import { BsArrowLeft } from 'react-icons/bs'; // Import de l'icône de flèche vers la gauche depuis React-icons
+import { Link } from 'react-router-dom'; // Import de la fonction Link de react-router-dom pour la navigation
 
-const BackButton = ({ destination = '/beneficiaire' }) => {
+const BackButton = ({ destination = '/solde' }) => { // Composant BackButton prenant une prop destination, par défaut '/solde'
     return (
         <div className='flex'>
             <Link
-                to={destination}
-                className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'
+                to={destination} // Lien vers la destination spécifiée
+                className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit' // Classes CSS pour le style du bouton
             >
                 <BsArrowLeft className='text-2xl' />
             </Link>
@@ -14,4 +14,4 @@ const BackButton = ({ destination = '/beneficiaire' }) => {
     );
 };
 
-export default BackButton;
+export default BackButton; // Export du composant BackButton
