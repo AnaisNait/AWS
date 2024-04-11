15b9c715357112f17581e190e811ddd64dc67d4a
uvsq22307849 <boualirami850@gmail.com>
2024-04-11 23:58:16 +0200

    transférer les routes dans un nouveau route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,15 +1,16 @@
 import express from "express";
-import { afficherUser, login, logout, register } from "../controllers/auth.js";
-import checkAuth from '../middlewares/check-auth.js';
+import { afficherUser, login, logout } from "../controllers/user.js"; // Import des contrôleurs pour les opérations utilisateur
+import checkAuth from '../middlewares/check-auth.js'; // Middleware pour vérifier l'authentification
 
-const authRouter = express.Router();
+const userRouter = express.Router(); // Création du routeur pour les utilisateurs
 
-authRouter.post('/login', login,checkAuth);
+// Route pour la connexion des utilisateurs
+userRouter.post('/login', login, checkAuth);
 
-authRouter.post('/register', register);
+// Route pour afficher les détails de l'utilisateur
+userRouter.get('/afficherUser', afficherUser, checkAuth);
 
-authRouter.post('/logout', logout);
+// Route pour la déconnexion des utilisateurs
+userRouter.post('/logout', logout);
 
-authRouter.get('/afficherUser',afficherUser,checkAuth)
-
-export default authRouter
+export default userRouter; // Export du routeur utilisateur
